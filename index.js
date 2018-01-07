e2dc9110faf3cf65776faddaaba185fd27705929
Adrian Juszczak <adrian@juszczak.io>
2018-01-08 00:30:29 +0100

    Static files
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var express = require('express')
+var path = require('path')
 
 var PORT = 3000;
 
 var app = express();
 
-app.get('*', function(req, res) {
-  res.json({
-    hello: 'world'
-  })
-})
+app.get('*', express.static(path.resolve(__dirname, 'dist')));
 
 app.listen(PORT, function() {
   console.log('listening on port', PORT);
