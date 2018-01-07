8f22ae4cb0fbbf0fb3ea4c443e0f29fb202156ba
Adrian Juszczak <adrian@juszczak.io>
2018-01-08 00:48:49 +0100

    Parallel script and static files proxy
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express')
+var proxy = require('express-http-proxy')
 var path = require('path')
 
 var PORT = 3000;
 
 var app = express();
 
-app.get('*', express.static(path.resolve(__dirname, 'dist')));
+var frontend;
+if (process.env['PRODUCTION']) {
+  frontend = express.static(path.resolve(__dirname, 'dist'))
+} else {
+  frontend = proxy('http://localhost:8080')
+}
+app.get('*', frontend)
 
 app.listen(PORT, function() {
   console.log('listening on port', PORT);
