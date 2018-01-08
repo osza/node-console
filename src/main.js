cfe54086d69a00913a6d5ef0091689aac1b2572b
Adrian Juszczak <adrian@juszczak.io>
2018-01-08 01:30:21 +0100

    User interface for Notes
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+document.addEventListener('DOMContentLoaded', function() {
+  var content = document.getElementById('content');
+  var form = document.getElementById('todos');
+  var input = document.getElementById('todo');
+
+  function addNote(note) {
+    var element = document.createElement('div')
+    element.innerHTML = note;
+    element.className = 'note'
+    content.appendChild(element);
+  }
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var note = input.value;
+    input.value = '';
+    addNote(note);
+  });
+});
